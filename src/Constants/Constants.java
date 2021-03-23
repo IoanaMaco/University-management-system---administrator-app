69851228dfef64865afc9a24395477571f9af914
andreitraistaru <56828222+andreitraistaru@users.noreply.github.com>
2021-03-23 18:06:56 +0200

    Second commit - background set; this commit is for testing purposes only

---
+++ src/Constants/Constants.java
@@ -0,0 +1,7 @@
+package Constants;
+
+import javafx.scene.paint.Color;
+
+public class Constants {
+    public static Color backgroundColor = Color.LIGHTBLUE;
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,21 +1,31 @@
 package sample;
 
+import Constants.Constants;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class Main extends Application {
+    private void initScene(Stage stage) {
+        stage.getScene().setFill(Constants.backgroundColor);
+        stage.setMaximized(true);
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("University Management System");
+        primaryStage.setScene(new Scene(root));
+
+        initScene(primaryStage);
+
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
